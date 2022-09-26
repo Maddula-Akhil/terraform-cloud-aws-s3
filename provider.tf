fe2117f2cdb454525f34a942daf384a4bf3598b6
Maddula-Akhil <107178987+Maddula-Akhil@users.noreply.github.com>
2022-09-26 13:44:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,25 +1,13 @@
 terraform {
-	
-	required_version = "~> 1.3.0"
-	
-	required_providers {
-	
-	aws = {
-	
-	source = "hashicorp/aws"
-	
-	version = "~> 4.24.0"
-	
-	}
-	
-	}
-	
-	}
-	
-	
-	
-	provider "aws" {
-	
-	region = var.aws_region
-	
-	}
+  required_version = "~> 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.24.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
