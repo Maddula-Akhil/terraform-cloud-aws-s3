7ef04f01e49136257c04c20f94163ddb5b64d407
akhilamp0000@gmail.com <akhilamp0000@gmail.com>
2023-01-20 06:31:25 +0000

    edited variables

--- variable.tf
+++ variable.tf
@@ -5,3 +5,13 @@ variable "aws_region" {
 	description = "AWS Region in which resources are provisioned"
 	
 	}
+variable "instance_type" {
+
+         type = string
+
+         description = "my instance"
+
+  }
+
+#Hi 
+#Hello
