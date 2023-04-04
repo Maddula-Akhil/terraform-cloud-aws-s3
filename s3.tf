2ff9b1640c7e3e924230755590e4adc506bae82f
root <root@ip-172-31-81-144.ec2.internal>
2023-04-04 05:48:13 +0000

    Creating S3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket.akhil"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#resource "aws_s3_bucket" "b" {
+#  bucket = "my-tf-test-bucket.akhil"
+#
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
+#}
