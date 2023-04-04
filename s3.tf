f8224f0a8564fa785dc0fc3b2fca2b4bbc21ab4d
root <root@ip-172-31-81-144.ec2.internal>
2023-04-04 05:44:55 +0000

    Creating S3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,8 @@
-#data "aws_s3_bucket" "public" {
-#  bucket = "terraform-cloud-awss3"
-#}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket.akhil"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
